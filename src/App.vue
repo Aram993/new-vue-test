fb4b0476c80b5f5a3fd37c667272c05d949ff16b
Aram993 <aram993@mail.ru>
2025-04-26 17:42:12 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+  import First from './components/First.vue'
+  export default {
+    name: "App",
+    components: {First},
+  }
+</script>
+
+<template>
+  <First/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/First.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="root">
+        Hello
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "First",
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
